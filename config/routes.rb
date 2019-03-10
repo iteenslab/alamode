02c372c704969f6b4623a9b915a24aeac4236a1d
arupakan125 <aru@arupaka.net>
2019-03-10 10:31:50 +0900

    クラス名をloginからsignupに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/', to: 'home#top'
-  get '/login', to: 'home#login'
+  get '/signup', to: 'home#signup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
